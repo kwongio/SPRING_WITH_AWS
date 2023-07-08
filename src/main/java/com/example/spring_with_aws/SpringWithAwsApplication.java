b690c69924465460ca689fd43b6c098ff628f535
kwongio <rldh11111@naver.com>
2023-07-08 22:02:57 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_with_aws/SpringWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_with_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithAwsApplication.class, args);
+    }
+
+}
